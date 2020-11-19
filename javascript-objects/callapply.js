3e1fa5d3f1c0e24490eaa333c2566a76b0938ce9
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-11-19 22:10:20 +0600

    Create callapply.js

---
+++ javascript-objects/callapply.js
@@ -0,0 +1,15 @@
+const person={
+    fname:"Zahid",
+    lname:"Rahman",
+    salary:4000,
+    getName:function(){
+        return `${this.fname} ${this.lname}`
+    },
+    setSalary:function(amount){
+        this.salary-=amount;
+        return this.salary;
+    }
+}
+console.log(person.getName);
+console.log(person.setSalary(150));
+console.log(person.salary);
