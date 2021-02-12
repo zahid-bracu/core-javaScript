34b424b78968d1163f702a807dbc1e44cbb432af
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2021-02-13 00:57:38 +0600

    up

---
+++ javascript-objects-methods/join-object.js
@@ -0,0 +1,14 @@
+var info={
+    mail:"zahid.mym@gmail.com",
+    id:15101122
+}
+
+
+var address={
+    road:"AGB Colony",
+    city:"Dhaka"
+}
+
+
+var joint={...info,...address};
+console.log(joint);
