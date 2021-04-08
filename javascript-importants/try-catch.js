011b396ba008eee82226168040a955c4a0bb3d55
Muhammad Zahidur Rahman <zahid.mym@gmail.com>
2021-04-08 15:09:26 +0600

    Create try-catch.js

---
+++ javascript-importants/try-catch.js
@@ -0,0 +1,10 @@
+
+try{
+    console.log("before in try block")
+    var x=10*y;
+    console.log(x);
+}catch(err){
+    console.log("before in catch block")
+    console.log("Error Occured");
+    console.log("After in try block")
+}
