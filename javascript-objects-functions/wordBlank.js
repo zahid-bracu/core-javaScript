6f129d98102ffa8d44ee57270f282dec922c50b5
zahid-bracu <zahid.mym@gmail.com>
2021-12-13 22:06:47 +0600

    ok

---
+++ javascript-objects-functions/wordBlank.js
@@ -0,0 +1,9 @@
+function wordBlanks(myAdjective,myNoun,myVerb,myAdverb){
+    var result="";
+    result+="The "+myAdjective+" "+myNoun+" "+myVerb+" "+"to store"+" "+myAdverb;
+    console.log(result);
+}
+
+wordBlanks("big","dog","running","fast");
+wordBlanks("small","cat","walking","slowly");
+
